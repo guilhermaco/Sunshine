317888c3c0e02090b2c482143e0deeb262d6651d
Guilherme Matheus Corrêa <guilhermaco_91@hotmail.com>
2015-02-14 23:50:11 -0300

    Lesson 1

--- app/src/main/java/com/example/guilherme/sunshine/MainActivity.java
+++ app/src/main/java/com/example/guilherme/sunshine/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.guilherme.sunshine;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,7 +8,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -60,6 +64,32 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            String[] forecastArray = {
+                    "Today - Sunny - 35º/25º",
+                    "Tomorrow - Rainy - 30º/22º",
+                    "Wednesday - Cloudy - 29º/25º",
+                    "Thursday - Sunny - 36º/26º",
+                    "Friday -  Rainy - 27º/22º",
+                    "Saturday - Rainy - 29º/24º",
+                    "Sunday - Sunny - 35º/25º"
+            };
+            //Storing the raw data in an arrayList
+            List<String> weekForecast = new ArrayList<>(Arrays.asList(forecastArray));
+            ArrayAdapter<String> mForecastAdapter = new ArrayAdapter<>(
+                    // The current context (this fragment parent activity)
+                    getActivity(),
+                    // ID of list item LAYOUT
+                    R.layout.list_item_forecast,
+                    // ID of the textview to populate
+                    R.id.list_item_forecast_textview,
+                    // Data to populate the textview
+                    weekForecast);
+
+            // Getting reference to the listview from the fragment_main.xml and attaching the adapter to it
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(mForecastAdapter);
+
             return rootView;
         }
     }
