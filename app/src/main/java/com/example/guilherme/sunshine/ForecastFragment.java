71be17182e05a3286b7588911f8073bad68b56de
Guilherme Matheus Corrêa <guilhermaco_91@hotmail.com>
2015-02-16 20:20:58 -0300

    Lesson 2 - Refresh Button (Postal code parameter and URI Builder Refactor)

--- app/src/main/java/com/example/guilherme/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/guilherme/sunshine/ForecastFragment.java
@@ -40,7 +40,7 @@ public class ForecastFragment extends Fragment {
         int id = item.getItemId();
         if (id == R.id.action_refresh) {
             FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute();
+            weatherTask.execute("51030580");
             return true;
         }
         return super.onOptionsItemSelected(item);
